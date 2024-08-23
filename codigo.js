b7ce5dab23cd53058e82218c13916efc6e76a4d7
byron1992 <bybolade@gmail.com>
2024-08-22 21:34:42 -0500

    completando el css y agregando algo de JavaScript

---
+++ codigo.js
@@ -0,0 +1,9 @@
+const ingresoTexto =document.getElementById("ingresoTexto");
+
+const botonEncriptar =document.getElementById("botonEncriptar");
+
+const botonDesencriptar =document.getElementById("botonDesencriptar");
+
+const botonCopiar =document.getElementById("botonCopiar");
+
+const mensajeFinal =document.getElementById("mensajeFinal");
