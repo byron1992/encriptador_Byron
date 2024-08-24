f2604b4b25b0bd73a70f5b0ee4a525670301c0b6
byron1992 <bybolade@gmail.com>
2024-08-23 21:26:16 -0500

    Boton encriptar completo

--- codigo.js
+++ codigo.js
@@ -7,3 +7,35 @@ const botonDesencriptar =document.getElementById("botonDesencriptar");
 const botonCopiar =document.getElementById("botonCopiar");
 
 const mensajeFinal =document.getElementById("mensajeFinal");
+
+const muneco =document.getElementById("muneco");
+
+const derechaInfo =document.getElementById("derechaInfo");
+
+const derecha =document.getElementById("derecha");
+let remplazar= [
+    ["e", "enter"],
+    ["o", "ober"],
+    ["a", "ai"],
+    ["i", "imes"],
+    ["u", "ufat"],
+]
+
+botonEncriptar.addEventListener("click", ()=>{
+    const texto = ingresoTexto.value.toLowerCase()
+    function encriptar(newText){
+        for(let i=0; i< remplazar.length; i++){
+            if (newText.includes(remplazar[i][0])){
+                newText = newText.replaceAll(remplazar[i][0],remplazar[i][1]);
+            };
+        };
+        return newText
+    }
+    const textoEncriptado =encriptar(texto);
+    mensajeFinal.innerHTML=textoEncriptado;
+    muneco.style.display="none";
+    derechaInfo.style.display="none";
+    botonCopiar.style.display="block";
+    derecha.classList.add("ajustar");
+    mensajeFinal.classList.add("ajustar");
+})
